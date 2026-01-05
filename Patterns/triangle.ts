02d0e3290fb5f3ee86a70b8175dee09bd18bbde0
Zunaid <ja7667924@gmail.com>
2026-01-05 17:41:56 +0530

    soled triangle patern with diffrent outcome

--- Patterns/triangle.ts
+++ Patterns/triangle.ts
@@ -1,9 +1,17 @@
 const n: number = 4;
 
+// for (let i = 0; i < n; i++) {
+//     let p: string = "";
+//     for (let j = 0; j <= i; j++) {
+//         p += String.fromCharCode(65 + i);
+//     }
+//     console.log(p);
+// }
+
 for (let i = 0; i < n; i++) {
     let p: string = "";
-    for (let j = 0; j <= i; j++) {
-        p += String.fromCharCode(65 + i);
+    for (let j = 1; j <= i + 1; j++) {
+        p += j;
     }
     console.log(p);
 }

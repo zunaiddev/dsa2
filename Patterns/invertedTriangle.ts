29b9df0ce1872bd495449cbf08d233efb57f7796
Zunaid <ja7667924@gmail.com>
2026-01-05 18:38:22 +0530

    Solved Inverted Triangle Pattern

---
+++ Patterns/invertedTriangle.ts
@@ -0,0 +1,15 @@
+const n: number = 4;
+
+for (let i = 0; i < n; i++) {
+    let p: string = "";
+
+    for (let j = 0; j <= i; j++) {
+        p += " ";
+    }
+
+    for (let j = 0; j < n - i; j++) {
+        p += i + 1;
+    }
+
+    console.log(p);
+}
